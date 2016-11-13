87d6d3450de4abdd580a9eaa66384dcb7f007571
JeGa <jega@win>
2016-11-13 18:58:22 +0100

    Added iterations for split finding.

--- dt.py
+++ dt.py
@@ -1,18 +1,37 @@
 import logging
 import numpy as np
 
+
 def split(data, feature):
+    # Get the best split for all possible splits.
+
     # Sort data based on one feature
     logging.info("Split feature " + str(feature))
-    dsorted = data[np.argsort(data[:,feature])]
-    print(dsorted)
+    dsorted = data[np.argsort(data[:, feature])]
 
     # Split point
     for i in range(dsorted.shape[0] - 1):
-        d1 = dsorted[i+1,feature]
-        d2 = dsorted[i,feature]
-        split = d1 + (d1 - d2)/2
-        print(split)
+        d1 = dsorted[i + 1, feature]
+        d2 = dsorted[i, feature]
+        split = d1 + (d1 - d2) / 2
+        logging.info("Split point: " + str(i) + " : " + str(split))
+
+        # TODO: Calculate how good the split is.
+
+
+def bestsplit(data):
+    # Search the best split over all features and possible splits.
+
+    dpoints, features = data.shape
+    features -= 1
+    logging.info("Find best split of " + str(dpoints) + " data points.")
+
+    for f in range(features):
+        split(data, f)
+
+
+def buildtree():
+    pass
 
 
 def loadcsv(filename):
@@ -20,14 +39,15 @@ def loadcsv(filename):
     logging.info("Loaded csv file " + filename +
                  " with: " +
                  "data points: " + str(data.shape[0]) +
-                 " features: " + str(data.shape[1]))
+                 ", features: " + str(data.shape[1] - 1))
     return data
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     data = loadcsv("02_homework_dataset.csv")
-    split(data, 0)
+
+    bestsplit(data)
 
 
 if __name__ == '__main__':

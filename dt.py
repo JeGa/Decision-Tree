cf38d96db5ee3751c3420c41ce1b81f1c89f858e
JeGa <jega@win>
2016-11-20 13:20:40 +0100

    Added classify and accuracy function.

--- dt.py
+++ dt.py
@@ -11,7 +11,7 @@ import os
 
 class DecisionTree:
     # How many points are used for the test set (percentage).
-    TEST = 0.3
+    TEST = 0.2
 
     def __init__(self, data, depth, qmeasure):
         # Complete data set with ground truth.
@@ -19,15 +19,17 @@ class DecisionTree:
         self.maxdepth = depth
         self.qmeasure = qmeasure
 
+        self.root = None
+        self.depth = 0
+
         _, self.dim, _, self.classes = self.datasetinfo(data)
 
         # Split in training and test set.
-        #nv = int(DecisionTree.TEST * data.shape[0])
-        #self.Xs, self.Xt = np.vsplit(self.data, [nv])
+        nv = int(DecisionTree.TEST * data.shape[0])
+        self.Xs, self.Xt = np.vsplit(self.data, [nv])
 
-        #logging.info("Training: " + str(self.Xt.shape[0]))
-        #logging.info("Test: " + str(self.Xs.shape[0]))
-        self.Xt = self.data
+        logging.info("Training: " + str(self.Xt.shape[0]))
+        logging.info("Test: " + str(self.Xs.shape[0]))
 
         self.buildtree()
 
@@ -42,7 +44,7 @@ class DecisionTree:
 
         return dpoints, col, col, int(classes) + 1
 
-    def makenode(self, data, feature, splitvalue):
+    def makenode(self, data):
         # Creates node and adds default values for the required attributes.
         node = ete3.TreeNode()
 
@@ -52,12 +54,17 @@ class DecisionTree:
         qvalue = self.qmeasure(classdist)
 
         node.add_features(data=data,
-                          feature=feature,
-                          le=splitvalue,
                           classdist=classdist,
-                          qvalue=qvalue)
+                          qvalue=qvalue,
+                          feature=None,
+                          le=None)
 
         return node
+
+    @staticmethod
+    def setsplit(node, f, splitvalue):
+        node.feature = f
+        node.le = splitvalue
 
     def classdist(self, data):
         classdist = dict()
@@ -81,7 +88,8 @@ class DecisionTree:
 
         return dist
 
-    def di(self, node, lnode, rnode):
+    @staticmethod
+    def di(node, lnode, rnode):
         """
         Improvement of a split.
         """
@@ -94,24 +102,25 @@ class DecisionTree:
         return node.qvalue - lp * lnode.qvalue - rp * rnode.qvalue
 
     def buildtree(self):
-        self.root = self.makenode(self.Xt, None, None)
-
         self.depth = 0
+        self.root = self.makenode(self.Xt)
         self._build(self.root)
 
     def _build(self, node):
         """
         DFS
         """
+        # TODO: Depth should checked for each node separately.
         if self.depth == self.maxdepth:
+            self.addtext(node)
             return
+
         lnode, rnode = self.bestsplit(node)
+        self.addtext(node)
 
-        if lnode == None or rnode == None:
+        if lnode is None or rnode is None:
             return
 
-        self.addtext(lnode)
-        self.addtext(rnode)
         node.add_child(rnode, 'r')
         node.add_child(lnode, 'l')
 
@@ -121,29 +130,14 @@ class DecisionTree:
         self._build(rnode)
 
     def addtext(self, node):
-        txt = str(self.classdist(node.data)) + os.linesep + str(node.qvalue)
-        # txt = np.array_str(node.data)
+        txt = "Classes: " + str(self.classdist(node.data)) + os.linesep + \
+              "Gini: " + str(node.qvalue)
+
+        txt += os.linesep + "Split: " + str(node.le) + ", " + str(node.feature)
 
         text = ete3.TextFace(txt)
 
-        # hola.margin_top = 10
-        # hola.margin_right = 10
-        # hola.margin_left = 10
-        # hola.margin_bottom = 10
-        # hola.opacity = 0.5  # from 0 to 1
-        # hola.inner_border.width = 1  # 1 pixel border
-        # hola.inner_border.type = 1  # dashed line
-        # hola.border.width = 1
-        # text.background.color = "LightGreen"
-
-        node.add_face(text, column=0, position="branch-right")
-
-    def split(self, node, f, split):
-        data = node.data
-        lnode = self.makenode(data[data[:, f] <= split], f, split)
-        rnode = self.makenode(data[data[:, f] > split], f, split)
-
-        return lnode, rnode
+        node.add_face(text, column=0, position="branch-top")
 
     def bestsplit(self, node):
         """
@@ -151,7 +145,6 @@ class DecisionTree:
 
         :return: left node and right node or None if there is no improvement.
         """
-
         data = node.data
 
         logging.info("Find best split of " + str(data.shape[0]) + " data points.")
@@ -190,13 +183,45 @@ class DecisionTree:
         else:
             return None, None
 
+    def split(self, node, f, split):
+        data = node.data
+
+        self.setsplit(node, f, split)
+
+        lnode = self.makenode(data[data[:, f] <= split])
+        rnode = self.makenode(data[data[:, f] > split])
+
+        return lnode, rnode
+
     def classify(self, x):
-        pass
-        # TODO
+        return self._classify(self.root, x)
+
+    def _classify(self, node, x):
+        feature = node.feature
+        splitvalue = node.le
+
+        value = x[feature]
+
+        if value <= splitvalue:
+            child = node.get_leaves_by_name('l')
+        else:
+            child = node.get_leaves_by_name('r')
+
+        if child[0].is_leaf():
+            return max(child[0].classdist, key=child[0].classdist.get)
+        else:
+            return self._classify(child[0], x)
 
     def accuracy(self):
-        pass
-        # TODO
+        points = self.Xt.shape[0]
+        correct = 0
+
+        for i in self.Xt:
+            c = self.classify(i)
+            if c == i[self.dim]:
+                correct += 1
+
+        return correct / points
 
     def show(self):
         self.root.show()
@@ -210,17 +235,16 @@ def gini(classdist):
         value = number of points of that class.
     :return: Gini index
     """
-    gini = 0
+    gval = 0
     for c in classdist.values():
-        gini += pow(c, 2)
-    gini = 1 - gini
-    return gini
+        gval += pow(c, 2)
+    gval = 1 - gval
+    return gval
 
 
 def loadcsv(filename):
     data = np.loadtxt(filename, delimiter=',', skiprows=1)
-    logging.info("Loaded csv file " + filename +
-                 " with: " +
+    logging.info("Loaded csv file " + filename + " with: " +
                  "data points: " + str(data.shape[0]) + ", " +
                  "features: " + str(data.shape[1] - 1))
     return data
@@ -233,6 +257,14 @@ def main():
 
     dtree = DecisionTree(data, depth=2, qmeasure=gini)
 
+    # Classify two samples
+    xa = np.array([4.1, -0.1, 2.2])
+    xb = np.array([6.1, 0.4, 1.3])
+
+    logging.info("xa = " + str(dtree.classify(xa)))
+    logging.info("xb = " + str(dtree.classify(xb)))
+
+    logging.info("Accuracy for test set: " + str(dtree.accuracy()))
     dtree.show()
 
 
